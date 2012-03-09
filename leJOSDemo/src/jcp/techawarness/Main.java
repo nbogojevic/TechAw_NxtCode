d370fb630650adf2a69f9e36aa5e7d75117d752d
Nenad Bogojevic <nbogojevic@hotmail.com>
2012-03-09 15:03:19 +0100

    leJOS demo skeletons

---
+++ leJOSDemo/src/jcp/techawarness/Main.java
@@ -0,0 +1,7 @@
+package jcp.techawarness;
+
+public class Main {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ leJOSDemo/src/jcp/techawarness/MovementBehavior.java
@@ -0,0 +1,25 @@
+package jcp.techawarness;
+
+import lejos.robotics.subsumption.Behavior;
+
+public class MovementBehavior implements Behavior {
+
+  @Override
+  public void action() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void suppress() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean takeControl() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
